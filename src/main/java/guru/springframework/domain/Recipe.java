32c090337ff45b55d682cc5b96e29704cb175afd
Adam Hamar <adam.hamar@gmail.com>
2021-06-13 19:21:06 +0200

    Add one-to-many relationship between Recipe and Notes

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,10 @@ public class Recipe {
     private String directions;
     // TODO: 6/13/2021 add
     // private Difficulty difficulty
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+    
     @Lob
     private Byte[] image;
     
@@ -86,6 +91,14 @@ public class Recipe {
     
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+    
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
     
     public Byte[] getImage() {
