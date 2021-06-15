1606fcfa0e6585a20c81189d9221aad349a5e558
Adam Hamar <adam.hamar@gmail.com>
2021-06-15 10:22:42 +0200

    Add Recipe service

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,18 +17,18 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
-    private String directions;
-    // TODO: 6/13/2021 add
-    // private Difficulty difficulty
-    
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
     
     @Lob
-    private Byte[] image;
+    private String directions;
     
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
+    
+    @Lob
+    private Byte[] image;
     
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -37,7 +38,7 @@ public class Recipe {
         name = "recipe_category",
         joinColumns = @JoinColumn(name = "recipe_id"),
         inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
     
     public Long getId() {
         return id;
