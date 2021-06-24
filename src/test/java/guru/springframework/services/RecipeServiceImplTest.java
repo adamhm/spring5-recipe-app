3295e45c091a11e2888bfbe2c141d544f3153552
Adam Hamar <adam.hamar@gmail.com>
2021-06-24 20:08:41 +0200

    Implement recipe deleting

--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -86,6 +86,21 @@ class RecipeServiceImplTest {
         verify(recipeRepository, never()).findAll();
     }
     
+    @Test
+    public void testDeleteById() throws Exception {
+        
+        //given
+        Long idToDelete = Long.valueOf(2L);
+        
+        //when
+        recipeService.deleteById(idToDelete);
+        
+        //no 'when', since method has void return type
+        
+        //then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
+    
     @AfterEach
     public void release() throws Exception {
         closeable.close();
