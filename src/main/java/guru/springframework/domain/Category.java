e61575f4f7ddf13d0f49b0a8716133f458813858
Adam Hamar <adam.hamar@gmail.com>
2021-06-25 11:22:02 +0200

    Add ingredient creating

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,12 +1,15 @@
 package guru.springframework.domain;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @Entity
 public class Category {
     
